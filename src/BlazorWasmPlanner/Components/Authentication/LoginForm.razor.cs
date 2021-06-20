1d71e9999c1446bfb5f267b7f3f3bf84f784fee5
nickguerra <nickg@maliciousgames.com>
2021-06-20 07:00:05 -0500

    started login form to wire up request

---
+++ src/BlazorWasmPlanner/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,24 @@
+﻿using BlazorWasmPlanner.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Components
+{
+    public partial class LoginForm : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new LoginRequest();
+
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
