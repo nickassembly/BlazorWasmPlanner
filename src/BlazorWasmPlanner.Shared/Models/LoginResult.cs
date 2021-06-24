95846d424c9b0d810a5220312239a721899014a7
nickguerra <nickg@maliciousgames.com>
2021-06-23 19:27:46 -0500

    Started creating login function

---
+++ src/BlazorWasmPlanner.Shared/Models/LoginResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Shared.Models
+{
+    public class LoginResult
+    {
+        public string Token { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+    }
+}
--- src/BlazorWasmPlanner/Components/Authentication/LoginForm.razor.cs
+++ src/BlazorWasmPlanner/Components/Authentication/LoginForm.razor.cs
@@ -1,9 +1,12 @@
-﻿using BlazorWasmPlanner.Shared.Models;
+﻿using Blazored.LocalStorage;
+using BlazorWasmPlanner.Shared.Models;
+using BlazorWasmPlanner.Shared.Responses;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,11 +17,42 @@ namespace BlazorWasmPlanner.Components
         [Inject]
         public HttpClient HttpClient { get; set; }
 
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Inject]
+        public JwtAuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        public ILocalStorageService Storage { get; set; }
+
         private LoginRequest _model = new LoginRequest();
+        private bool _isBusy = false;
+        private string _errorMessage = string.Empty;
 
         private async Task LoginUserAsync()
         {
-            throw new NotImplementedException();
+            _isBusy = true;
+            _errorMessage = string.Empty;
+
+            var response = await HttpClient.PostAsJsonAsync("/api/v2/auth/login", _model);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<ApiResponse<LoginResult>>();
+                // Store in local storage
+                await Storage.SetItemAsStringAsync("access_token", result.Value.Token);
+                await Storage.SetItemAsync<DateTime>("expiry_date", result.Value.ExpiryDate);
+
+                await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            }
+            else
+            {
+                var errorResult = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                _errorMessage = errorResult.Message;
+            }
+
+            _isBusy = false;
         }
     }
 }
