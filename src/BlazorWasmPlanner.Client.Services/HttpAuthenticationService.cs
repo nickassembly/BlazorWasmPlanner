769ed1f1f0e17aa1669a0fb3b2f9e8d14d5cf2e0
nickguerra <nickg@maliciousgames.com>
2021-06-28 18:55:41 -0500

    added services project and user register service

---
+++ src/BlazorWasmPlanner.Client.Services/HttpAuthenticationService.cs
@@ -0,0 +1,41 @@
+﻿using BlazorWasmPlanner.Client.Services.Exceptions;
+using BlazorWasmPlanner.Client.Services.Interfaces;
+using BlazorWasmPlanner.Shared.Models;
+using BlazorWasmPlanner.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Client.Services
+{
+    public class HttpAuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+
+        public HttpAuthenticationService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<ApiResponse> RegisterUserAsync(RegisterRequest model)
+        {
+            var response = await _client.PostAsJsonAsync("/api/v2/auth/register", model);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<ApiResponse>();
+                return result;
+            }
+            else
+            {
+                var errorResponse = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                throw new ApiException(errorResponse, response.StatusCode);
+            }
+
+        }
+    }
+}
---
+++ src/BlazorWasmPlanner.Client.Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorWasmPlanner.Shared.Models;
+using BlazorWasmPlanner.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Client.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task<ApiResponse> RegisterUserAsync(RegisterRequest model);
+
+        // TODO: migration login to IAuthenticationService
+    }
+}
