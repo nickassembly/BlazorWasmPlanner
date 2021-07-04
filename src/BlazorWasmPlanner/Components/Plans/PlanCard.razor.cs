958a33d79aa23b52d33d5a2d7f1b86c8b18abc87
nickguerra <nickg@maliciousgames.com>
2021-07-04 05:45:09 -0500

    finished plans list layout component

---
+++ src/BlazorWasmPlanner/Components/Plans/PlanCard.razor.cs
@@ -0,0 +1,30 @@
+﻿using BlazorWasmPlanner.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Components
+{
+    public partial class PlanCard
+    {
+        [Parameter]
+        public bool IsBusy { get; set; }
+
+        [Parameter]
+        public PlanSummary PlanSummary { get; set; }
+
+        [Parameter]
+        public EventCallback<PlanSummary> OnViewClicked { get; set; }
+
+        [Parameter]
+        public EventCallback<PlanSummary> OnDeleteClicked { get; set; }
+
+        [Parameter]
+        public EventCallback<PlanSummary> OnEditClicked { get; set; }
+
+
+    }
+}
