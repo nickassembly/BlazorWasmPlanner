ebbbad7431ed207675c8667081b95637d66e3615
nickguerra <nickg@maliciousgames.com>
2021-07-09 06:36:11 -0500

    Added layout container and breadcrumb components

---
+++ src/BlazorWasmPlanner/Pages/Plans/Plans.razor.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Pages.Plans
+{
+    public partial class Plans
+    {
+        private List<BreadcrumbItem> _breadcrumbItems { get; set; } = new()
+        {
+            new BreadcrumbItem("Home", "/index"),
+            new BreadcrumbItem("Plans", "/plans", true),
+        };
+    }
+}
