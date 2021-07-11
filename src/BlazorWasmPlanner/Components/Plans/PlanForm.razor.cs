3655cd1c0a87dcc75ce57e5a106a397d028f3bb4
nickguerra <nickg@maliciousgames.com>
2021-07-11 06:27:12 -0500

    finished create form layout

---
+++ src/BlazorWasmPlanner/Components/Plans/PlanForm.razor.cs
@@ -0,0 +1,63 @@
+﻿using BlazorWasmPlanner.Client.Services.Interfaces;
+using BlazorWasmPlanner.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Components
+{
+    public partial class PlanForm
+    {
+        [Inject]
+        public IPlansService PlansService { get; set; }
+
+        private PlanDetail _model = new PlanDetail();
+        private bool _isBusy = false;
+        private Stream _stream = null;
+        private string _fileName = string.Empty;
+        private string _errorMessage = string.Empty;
+
+        private async Task SubmitFormAsync()
+        {
+            _errorMessage = "done";
+        }
+
+        private async Task OnChooseFileAsync(InputFileChangeEventArgs e)
+        {
+            _errorMessage = string.Empty;
+            var file = e.File;
+            if (file != null)
+            {
+               if (file.Size > 2097152)
+                {
+                    _errorMessage = "The file must be less than 2MB";
+                    return;
+                }
+
+                string[] allowedExtensions = new[] { ".jpg", ".png", ".bmp", ".svg" };
+
+                string extension = Path.GetExtension(file.Name).ToLower();
+
+                if (!allowedExtensions.Contains(extension))
+                {
+                    _errorMessage = "Please choose a valid image type (.jpg/.png/.bmp/.svg)";
+                    return;
+                }
+
+                using (var stream = file.OpenReadStream(2097152))
+                {
+                    var buffer = new byte[file.Size];
+                    await stream.ReadAsync(buffer, 0, (int)file.Size);
+                    _stream = new MemoryStream(buffer);
+                    _stream.Position = 0;
+                    _fileName = file.Name;
+                }
+            }
+        }
+    }
+}
---
+++ src/BlazorWasmPlanner/Pages/Plans/CreateEditPlan.razor.cs
@@ -0,0 +1,19 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Pages.Plans
+{
+    public partial class CreateEditPlan
+    {
+        private List<BreadcrumbItem> _breadcrumbItems = new()
+        {
+            new BreadcrumbItem("Home", "/index"),
+            new BreadcrumbItem("Plans", "/plans"),
+            new BreadcrumbItem("Create", "/plans/form", true)
+        };
+    }
+}
