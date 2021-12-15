52aa09dca95162a35d51532398afd041b8281d3a
nickguerra <nickg@maliciousgames.com>
2021-12-15 06:12:50 -0600

    Added dialog details for plan

---
+++ src/BlazorWasmPlanner/Components/Plans/PlanDetailsDialog.razor.cs
@@ -0,0 +1,55 @@
+﻿using BlazorWasmPlanner.Client.Services.Exceptions;
+using BlazorWasmPlanner.Client.Services.Interfaces;
+using BlazorWasmPlanner.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Components
+{
+    public partial class PlanDetailsDialog
+    {
+        [CascadingParameter]
+        MudDialogInstance MudDialog { get; set; }
+
+        [Inject]
+        public IPlansService PlansService { get; set; }
+
+        [Parameter]
+        public string PlanId { get; set; }
+
+        private PlanDetail _plan;
+        private bool _isBusy;
+        private string _errorMessage = String.Empty; 
+
+        private void Close()
+        {
+            MudDialog.Cancel();    
+        }
+
+        private async Task FetchPlanAsync()
+        {
+            _isBusy = true;
+
+            try
+            {
+                var result = await PlansService.GetByIdAsync(PlanId);
+                _plan = result.Value;
+            }
+            catch (ApiException ex)
+            {
+                // TODO
+            }
+            catch (Exception ex)
+            {
+                // Log this error
+            }
+            _isBusy = false;
+        }
+
+    }
+}
