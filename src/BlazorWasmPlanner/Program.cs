5ce9452866292a41dd28a4d0b9158f88cbd62baa
nickguerra <nickg@maliciousgames.com>
2021-12-20 18:40:15 -0600

    upgrade to .net 6

--- src/BlazorWasmPlanner/Program.cs
+++ src/BlazorWasmPlanner/Program.cs
@@ -1,39 +1,28 @@
 using Blazored.LocalStorage;
+using BlazorWasmPlanner;
 using BlazorWasmPlanner.Client.Services;
-using BlazorWasmPlanner.Client.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
 
-namespace BlazorWasmPlanner
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("PlannerApp.Api", client =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+builder.Services.AddTransient<AuthorizationMessageHandler>();
 
-            builder.Services.AddHttpClient("PlannerApp.Api", client =>
-            {
-                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
-            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
-            builder.Services.AddTransient<AuthorizationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
 
-            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+builder.Services.AddHttpClientServices();
 
-            builder.Services.AddMudServices();
-            builder.Services.AddBlazoredLocalStorage();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
-            builder.Services.AddHttpClientServices();
+await builder.Build().RunAsync();
 
-            await builder.Build().RunAsync();
-        }
-    }
-
-}
