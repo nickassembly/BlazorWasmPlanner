adedc469f17d6f1cf1aa82c2048a269661f09b75
nickguerra <nickg@maliciousgames.com>
2021-12-22 07:40:04 -0600

    Added checkbox and delete icon to todo component

--- src/BlazorWasmPlanner/Components/ToDoItems/ToDoItem.razor.cs
+++ src/BlazorWasmPlanner/Components/ToDoItems/ToDoItem.razor.cs
@@ -6,11 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorWasmPlanner.Components.ToDoItems
+namespace BlazorWasmPlanner.Components
 {
     public partial class ToDoItem
     {
         [Parameter]
         public ToDoItemDetail Item { get; set; }
+
+        private bool _isChecked = true;
+
+
     }
 }
