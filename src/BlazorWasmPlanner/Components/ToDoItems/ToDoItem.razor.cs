d5adff8bd3a1a6bf37207bf0750e5ae351fbc001
nickguerra <nickg@maliciousgames.com>
2021-12-22 05:10:37 -0600

    started working on todo card mud-blazor styles

---
+++ src/BlazorWasmPlanner/Components/ToDoItems/ToDoItem.razor.cs
@@ -0,0 +1,16 @@
+﻿using BlazorWasmPlanner.Shared.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWasmPlanner.Components.ToDoItems
+{
+    public partial class ToDoItem
+    {
+        [Parameter]
+        public ToDoItemDetail Item { get; set; }
+    }
+}
