e5c82ebe1663d6be1b9d829800eb03ee4d4144fa
nickguerra <nickg@maliciousgames.com>
2022-01-12 06:49:01 -0600

    Implemented Light/Dark theme switch

--- src/BlazorWasmPlanner/Shared/MainLayout.razor.cs
+++ src/BlazorWasmPlanner/Shared/MainLayout.razor.cs
@@ -17,10 +17,18 @@ namespace BlazorWasmPlanner.Shared
 
         protected async override Task OnInitializedAsync()
         {
-            _currentTheme = _darkTheme;
+            if(await LocalStorage.ContainKeyAsync("theme"))
+                _themeName = await LocalStorage.GetItemAsStringAsync("theme");
+            else
+                _themeName = "light";
+
+            _currentTheme = _themeName == "light" ? _lightTheme : _darkTheme;
+
         }
 
         MudTheme _currentTheme = null;
+
+        private string _themeName = "light";
 
         MudTheme _darkTheme = new MudTheme
         {
@@ -35,11 +43,45 @@ namespace BlazorWasmPlanner.Shared
                DrawerBackground = "#093958",
                DrawerText = "#FFFFFF",
                Surface = "#093958",
-               ActionDefault = "#0097FF",
+               ActionDefault = "#0C1217",
                ActionDisabled = "#2F678C",
                TextDisabled = "#B0B0B0"
-
            }
         };
+
+        MudTheme _lightTheme = new MudTheme
+        {
+            Palette = new Palette
+            {
+                AppbarBackground = "#0097FF",
+                AppbarText = "#FFFFFF",
+                Primary = "#007CD1",
+                TextPrimary = "#0C1217",
+                Background = "#F4FDFF",
+                TextSecondary = "#0C1217",
+                DrawerBackground = "#C5E5FF",
+                DrawerText = "#0C1217",
+                Surface = "#E4FAFF",
+                ActionDefault = "#0C1217",
+                ActionDisabled = "#2F678C",
+                TextDisabled = "#676767"
+            }
+        };
+
+        private async Task ChangeThemeAsync()
+        {
+            if (_themeName == "light")
+            {
+                _currentTheme = _darkTheme;
+                _themeName = "dark";
+            }
+            else
+            {
+                _currentTheme = _lightTheme;
+                _themeName = "light";
+            }
+
+            await LocalStorage.SetItemAsStringAsync("theme", _themeName);
+        }
     }
 }
